6a1562fe13782568a6f65a5ec0708304dd4dd167
maslevx <levic.mason@gmail.com>
2015-06-16 23:31:36 -0600

    new

---
+++ Utils/Observable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public abstract class Observable<T> : IObservable<T>
+    {
+        List<IObserver<T>> m_observers = new List<IObserver<T>>();
+
+        public IDisposable Subscribe(IObserver<T> observer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
